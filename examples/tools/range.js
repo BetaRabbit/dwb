5e83dbf8ca46899e7885c2528bc1bec405fa6116
Tianxiao <tianxiaom@citrix.com>
2017-02-09 15:47:31 +0800

    change examples request body to string

--- examples/tools/range.js
+++ examples/tools/range.js
@@ -7,9 +7,9 @@ const range = moment.range('2016-01-01', '2016-12-31')
 
 const days = Array.from(range.by('day')).map(d => ({
   path: '/api/sql',
-  body: {
-    sql: `select count(*) from xd_ceip_ceip_uploadinfo_view where ctime>='${d.format('YYYY-MM-DD')}' and ctime<'${d.add(1, 'days').format('YYYY-MM-DD')}'`
-  },
+  body: JSON.stringify({
+    sql: `select count(*) from xd_ceip_ceip_uploadinfo_view where cdate='${d.format('YYYY-MM-DD')}'`
+  }),
   method: 'POST',
   headers: {
     'Content-Type': 'application/json'
