d4f548554f0369a509de674626da4a066da5df46
Tianxiao <tianxiaom@citrix.com>
2017-04-14 11:14:30 +0800

    extend request size limit

--- src/index.js
+++ src/index.js
@@ -11,7 +11,8 @@ const port = process.env.PORT || 3000
 
 const app = express()
 app.use(cors())
-app.use(bodyParser.json())
+app.use(bodyParser.json({ limit: '50mb' }))
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
 app.use(morgan('combined', {
   stream: {
     write: msg => logger.info(msg.trim())
